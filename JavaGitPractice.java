e5b3be6ee7162a8730d487652df8dacc60a40505
ROSE MCCORMACK <mccormacksa@appstate.edu>
2024-05-03 19:45:17 -0400

    I will work on this over time

---
+++ JavaGitPractice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class JavaGitPractice
+{
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println("Hello, my name is Rose.");
+		System.out.println("This document over time will document files practice.");
+
+	}
+}
