8d6769313c41c20269e1b3e86e7c147303826f65
ROSE MCCORMACK <mccormacksa@appstate.edu>
2024-05-08 23:02:10 -0400

    This is the start of a teaching tool

---
+++ JavaCollectionsDriver.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class JavaCollectionsDriver
+{
+    /**
+    *@param args This is our main method where our data structures will be
+    *printed and where we will use different methods and operations on them.
+    *
+    *@throws IOException We will use File I/O on this project in the future.
+    */
+	public static void main(String[] args) throws IOException
+	{
+        LinkedList<Integer> struct_ONE = new ArrayList<>();
+        Set<Integer> struct_TWO = new HashSet<>();
+        Map<Integer, Integer> struct_THREE = new MashMap<>();
+        // Remember that we are using PriorityQueue
+        Queue<Integer> struct_FOUR = new PriorityQueue<>();
+        // Remember that we are using ArrayDeque
+        Deque<Integer> struct_FIVE = new ArrayDeque<>();
+        Stack<Integer> struct_SIX = new Stack<>();
+	}
+}
+
